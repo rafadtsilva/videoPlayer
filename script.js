af451297d48e4cac852107ef472b13bae0b8d5a9
Rafael Trindade <rafaeltrindade@outlook.com>
2021-07-17 10:35:22 -0300

    primeiro commit; adicionado video e icones que se tornaram os controles do player

---
+++ script.js
@@ -0,0 +1,23 @@
+// ========== AULA 01 E 02 ====================
+
+// let image = document.getElementById('like')
+
+// image.addEventListener('click', function () {
+//   image.setAttribute('src', 'images/deslike.jpg')
+// })
+
+// let lista = document.getElementById('lista')
+
+// lista.dataset.n = '10'
+
+// let num = parseInt(lista.dataset.n)
+
+// console.log()
+
+// let conteudo = ''
+
+// for (let i = 0; i < num; i++) {
+//   conteudo += '<li>' + i + '</li>'
+// }
+
+// lista.innerHTML = conteudo
